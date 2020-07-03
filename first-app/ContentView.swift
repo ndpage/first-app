34a42e4b1a7da38fc2f23363d71d81272b28370d
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-07-02 22:29:45 -0400

    Initial Commit

---
+++ first-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first-app
+//
+//  Created by Nathan Page on 7/2/20.
+//  Copyright © 2020 Nathan Page. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
